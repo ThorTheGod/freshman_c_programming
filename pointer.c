0dbae2da219f6c890d3bf6ca34145c8cc2fb324a
ThorTheGod <44338446+ThorTheGod@users.noreply.github.com>
2018-12-10 12:22:22 +0800

    Pointer.c
    learning note

---
+++ pointer.c
@@ -0,0 +1,6 @@
+The pointer's main point is address.
+For example,we can control the pointer's value--its address,which is actually 'variable''s address
+and is diffrerent from that pointer's address,to do something like change the 'variable''s value.
+&variable is a operation to get the variable's address.
+*pointer is a operation to get the variable's value of pointer's address.Obiviously,only pointer can use * to calculate.
+In addition,if we define 'int a' and 'int *p' 
